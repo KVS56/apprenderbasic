f5eb5de82bef2d4f2eac1aa8007dee7d2051127d
KVS <109676483+KVS56@users.noreply.github.com>
2025-11-24 20:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'hello world'
-
-@app.route('/')
 def flying_dutchman():
     return 'flying dutchman'
