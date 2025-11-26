e734db9b9b67cb790e847a7b766c848ce1ed5ef5
KVS <109676483+KVS56@users.noreply.github.com>
2025-11-26 21:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def flying_dutchman():
-    return 'flying dutchman'
+    return 'flying finn'
+    x = input('what is the current year?')
+def get_unix_time():
+    # Get current UTC datetime object
+    now_utc = datetime.utcnow()
+    unix_timestamp_years = now_utc.timestamp()
+    return  "unix_time_seconds": unix_timestamp_seconds
+    
 
 @app.route('/sister')
 def sheistywearer():
